744de8043bb7901b6558420928d28db77d3274b6
runicodec <runicodec@gmail.com>
2025-06-27 20:34:38 -0400

    use local directory for development

--- plugins/glacier.lua
+++ plugins/glacier.lua
@@ -1,4 +1,13 @@
-return {
-  "runicodec/glacier.nvim",
-  lazy = false,
-}
+if vim.fn.isdirectory(vim.fn.expand("~/Projects/glacier.nvim")) == 1 then
+  return {
+    name = "glacier.nvim",
+    enabled = true,
+    dir = vim.fn.expand("~/Projects/glacier.nvim"),
+    dev = true,
+  }
+else
+  return {
+    "runicodec/glacier.nvim",
+    enabled = true,
+  }
+end
